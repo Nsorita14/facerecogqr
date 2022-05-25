d76fb9abe7c441b5f3e64848125a490d45d64642
Patotie <nesha.sorita@gmail.com>
2022-05-25 17:35:33 +0800

    .

--- Exit.php
+++ Exit.php
@@ -8,30 +8,82 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
 
+body {
+	background-image: url("BGpic.jpg");
+	background-position: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	font-family: 'Montserrat', sans-serif;
+	height: 100vh;
+	margin: -20px 0 0px;
+}
+
 h1 {
 	font-weight: bold;
 	margin: 0;
 }
 
+
+td {
+	font-size: 18px;
+	font-weight: 100;
+	line-height: 20px;
+	letter-spacing: 0.5px;
+	margin: 20px 0 30px;
+}
+
+span {
+	margin: 10px 0 0px;
+	font-size: 14px;
+}
+
 button {
-    border-radius: 20px;
-    border: 1px solid #5DB1B9;
-    background-color: #5DB1B9;
-    color: #FFFFFF;
-    font-size: 14px;
-    font-weight: bold;
-    padding: 10px 45px;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    transition: transform 80ms ease-in;
-  }
-  
-  input {
+	border-radius: 20px;
+	border: 1px solid #5DB1B9;
+	background-color: #5DB1B9;
+	color: #FFFFFF;
+	font-size: 12px;
+	font-weight: bold;
+	padding: 12px 45px;
+	letter-spacing: 1px;
+	text-transform: uppercase;
+	transition: transform 80ms ease-in;
+}
+
+form {
+	background-color: #FFFFFF;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	padding: 0 50px;
+	height: 100%;
+	text-align: center;
+	margin: 0;
+	border-radius: 20px;
+}
+
+input {
 	background-color: #ccd5d5;
 	border: none;
 	border-radius: 15px;
 	padding: 12px 15px;
 	margin: 8px 0;
+	width: 100%;
+}
+
+.image {
+	margin-top: -2px;
+	margin-left: -35px;
+ }
+
+select {
+	background-color: #eee;
+	border-radius: 15px;
+	padding: 12px 15px;
+	margin: 6px 0;
 	width: 100%;
 }
 
@@ -47,13 +99,23 @@ button {
 	min-height: 480px;
 }
 
+.loginerror-msg{
+   background: #ffeeee;
+   color: #ba0200;
+   font-size: 12px;
+   padding: 5px;
+   width: 100%;
+   border-radius: 5px;
+   margin: 5px auto;
+}
+
 h1{
 
-padding: 15px;
-font-weight: 600;
-font-size: 30px;
-background: #5DB1B9;
-color: white
+	padding: 15px;
+	font-weight: 600;
+	font-size: 45px;
+	background: #5DB1B9;
+	color: white
 }
 
 
