d99b54889bdf1dfa406319b8ff1f6a1f536ae5a1
Patotie <nesha.sorita@gmail.com>
2022-05-25 17:45:33 +0800

    .

--- Exit.php
+++ Exit.php
@@ -9,7 +9,6 @@
     <style>
 
 body {
-	background-image: url("BGpic.jpg");
 	background-position: center;
 	display: flex;
 	justify-content: center;
@@ -25,20 +24,6 @@ h1 {
 	margin: 0;
 }
 
-
-td {
-	font-size: 18px;
-	font-weight: 100;
-	line-height: 20px;
-	letter-spacing: 0.5px;
-	margin: 20px 0 30px;
-}
-
-span {
-	margin: 10px 0 0px;
-	font-size: 14px;
-}
-
 button {
 	border-radius: 20px;
 	border: 1px solid #5DB1B9;
@@ -52,40 +37,6 @@ button {
 	transition: transform 80ms ease-in;
 }
 
-form {
-	background-color: #FFFFFF;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	padding: 0 50px;
-	height: 100%;
-	text-align: center;
-	margin: 0;
-	border-radius: 20px;
-}
-
-input {
-	background-color: #ccd5d5;
-	border: none;
-	border-radius: 15px;
-	padding: 12px 15px;
-	margin: 8px 0;
-	width: 100%;
-}
-
-.image {
-	margin-top: -2px;
-	margin-left: -35px;
- }
-
-select {
-	background-color: #eee;
-	border-radius: 15px;
-	padding: 12px 15px;
-	margin: 6px 0;
-	width: 100%;
-}
 
 .container {
 	background-color: #fff;
@@ -97,16 +48,6 @@ select {
 	width: 768px;
 	max-width: 100%;
 	min-height: 480px;
-}
-
-.loginerror-msg{
-   background: #ffeeee;
-   color: #ba0200;
-   font-size: 12px;
-   padding: 5px;
-   width: 100%;
-   border-radius: 5px;
-   margin: 5px auto;
 }
 
 h1{
