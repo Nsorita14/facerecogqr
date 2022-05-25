5c14746c0d873a23c0d35cee477ae12e020721a9
Patotie <nesha.sorita@gmail.com>
2022-05-25 17:44:51 +0800

    .

--- front.php
+++ front.php
@@ -4,7 +4,56 @@
 <style>
     body {font-family: Arial, Helvetica, sans-serif;}
 
-   
+    body {
+	background-position: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	font-family: 'Montserrat', sans-serif;
+	height: 100vh;
+	margin: -20px 0 0px;
+}
+
+h1 {
+	font-weight: bold;
+	margin: 0;
+}
+
+button {
+	border-radius: 20px;
+	border: 1px solid #5DB1B9;
+	background-color: #5DB1B9;
+	color: #FFFFFF;
+	font-size: 12px;
+	font-weight: bold;
+	padding: 12px 45px;
+	letter-spacing: 1px;
+	text-transform: uppercase;
+	transition: transform 80ms ease-in;
+}
+
+
+.container {
+	background-color: #fff;
+	border-radius: 10px;
+  	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
+			0 10px 10px rgba(0,0,0,0.22);
+	position: relative;
+	overflow: hidden;
+	width: 768px;
+	max-width: 100%;
+	min-height: 480px;
+}
+
+h1{
+
+	padding: 15px;
+	font-weight: 600;
+	font-size: 45px;
+	background: #5DB1B9;
+	color: white
+}
 
 
 
