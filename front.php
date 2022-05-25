0d7b990355f57c52f1f90fc509af30b8fb1290ce
Patotie <nesha.sorita@gmail.com>
2022-05-25 16:13:28 +0800

    remove

--- front.php
+++ front.php
@@ -30,19 +30,6 @@ button {
     transition: transform 80ms ease-in;
   }
 
-  .container {
-	background-color: #fff;
-	border-radius: 10px;
-  	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
-			0 10px 10px rgba(0,0,0,0.22);
-	position: relative;
-	overflow: hidden;
-	width: 768px;
-	max-width: 100%;
-	min-height: 480px;
-}
-
-
     </style></head>
 
      <body style="background-color:#92D2E2;">
