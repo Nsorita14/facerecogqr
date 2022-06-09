186cf383ab3259db2a83ec1cca1c8331b952e8a5
Jorge Michael Galang <jorgegalang21@gmail.com>
2022-06-09 15:47:25 +0800

    .

--- login_append.php
+++ login_append.php
@@ -1,6 +1,5 @@
 <?php
 include '../config.php';
-session_start();
 
 
 #Grab input values
@@ -9,29 +8,32 @@ $qr_lastname = $_POST["qr_lastname"];
 $qr_studentid = $_POST["qr_studentid"];
 $qr_course = $_POST["qr_course"];
 
-$qr_pin =  substr(md5(rand()), 0, 6);
+$qr_pin =  substr(md5(rand()), 0, 5);
 
 
 #Count the rows in the rgstrd_users and increment it by one
-$count_id = mysqli_query($conn, "SELECT COUNT(*) FROM pending_qr");
+$count_id = mysqli_query($conn, "SELECT COUNT(*) FROM `qr_pending-users`");
 $count_array = mysqli_fetch_array($count_id);
 $id = $count_array[0];
 $id = ++$id;
 
 
 #Replace to insert a data to dropped indexes
-$register = "REPLACE INTO `pending_qr` (qr_firstname,qr_lastname, qr_studentid, qr_course, qr_pin) 
-        VALUES ('$qr_firstname','$qr_firstname', '$qr_studentid', '$qr_course', '$qr_pin')";
+$register = "REPLACE INTO `qr_pending-users` (`count`, qr_firstname, qr_lastname, qr_studentid, qr_course, qr_pin) 
+                     VALUES ('$id', '$qr_firstname','$qr_firstname', '$qr_studentid', '$qr_course', '$qr_pin')";
 $rs = mysqli_query($conn, $register);
 
 
 
 if ($rs) {
-        $_SESSION['ru_name'] = "$qr_firstname";
-        $_SESSION['ru_pin'] = "$qr_pin";
-        $_SESSION['user_status'] = "successfully logged!";
-
+        session_start();
+        $_SESSION['id'] = $id;
+        $_SESSION['name'] = $qr_firstname;
+        $_SESSION['status'] = "successfully requested.";
         header('location: success.php');
 } else {
-        echo "Error has occured.";
+        echo ("<script LANGUAGE='JavaScript'>
+        window.alert('QR Registration failed. Please try again later.');
+        window.location.href='index.php';
+        </script>");
 }
